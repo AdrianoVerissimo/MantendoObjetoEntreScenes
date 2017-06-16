fcc9df42fc485c2c19035b0b506d545e01b595d9
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2017-06-16 17:48:20 -0300

    manter o objeto 'Caixa' entre as cenas, a partir do momento em que é encontrado na cena 'Fase1'.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		//verifica se está na cena de nome "Principal"
+		if (SceneManager.GetActiveScene ().name == "Fase1")
+		{
+			GameObject obj = GameObject.Find ("Caixa"); //procura pelo objeto de nome Caixa, que está somente na cena "Principal"
+			DontDestroyOnLoad (obj); //define que ele não será destruído ao alterar cenas
+
+			SceneManager.LoadScene ("Fase2"); //muda para a cena de nome "Secundaria"
+		}
+		else if(SceneManager.GetActiveScene ().name == "Fase2") //está na cena "Secundaria"
+		{
+			GameObject obj = GameObject.Find ("Caixa"); //procura pelo objeto "Caixa"
+			if (obj) //se encontrou objeto
+				Debug.Log ("Encontrou em: " + SceneManager.GetActiveScene().name); //mensagem que encontrou
+			else
+				Debug.Log ("Não encontrou em: " + SceneManager.GetActiveScene().name); //mensagem que não encontrou
+		}
+	}
+}
